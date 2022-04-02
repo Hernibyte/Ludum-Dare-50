5f4dcd0255394367f5ed052eae2a7406bec219ea
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-04-02 17:01:04 -0300

    Added check if end game

--- Assets/Hernan/GameManager.cs
+++ Assets/Hernan/GameManager.cs
@@ -7,25 +7,35 @@ public class GameManager : MonoBehaviour
     [SerializeField] float gameTime;
     [SerializeField] int points;
     [SerializeField] int excusesAmount;
- 
+
     const int maxExcuses = 10;
-    
+    bool ifEndGame = false;
+
     void Update()
     {
-        gameTime -= Time.deltaTime;
-        CheckTime();
+        if (!ifEndGame)
+        {
+            gameTime -= Time.deltaTime;
+            CheckTime();
+        }
     }
 
     void CheckTime()
     {
         if (gameTime <= 0)
+        {
             Debug.Log("bad end");
+            ifEndGame = true;
+        }
     }
 
     void CheckExcuses()
     {
         if (excusesAmount >= maxExcuses)
+        {
             Debug.Log("good end");
+            ifEndGame = true;
+        }
     }
 
     public void AddPoints(int point)
@@ -35,7 +45,10 @@ public class GameManager : MonoBehaviour
 
     public void AddExcuses(int excuse)
     {
-        excusesAmount += excuse;
-        CheckExcuses();
+        if (!ifEndGame)
+        {
+            excusesAmount += excuse;
+            CheckExcuses();
+        }
     }
 }
