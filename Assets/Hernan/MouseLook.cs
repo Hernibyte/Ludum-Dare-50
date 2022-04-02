298645e9154e02f4817e3ba209eb5e11172b31db
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-04-02 00:06:48 -0300

    Added player controller

---
+++ Assets/Hernan/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity;
+    [SerializeField] Transform playerBody;
+
+    float xRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * x);
+    }
+}
---
+++ Assets/Hernan/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float gravity = -9.81f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundLayerMask;
+
+    CharacterController characterController;
+    bool isGrounded;
+    public Vector3 velocity;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+        FisicsxD();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 newPos = transform.right * x + transform.forward * y;
+        characterController.Move(newPos);
+    }
+
+    void FisicsxD()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
