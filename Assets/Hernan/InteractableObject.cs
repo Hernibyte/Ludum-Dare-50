9ab44be513b98141fd0811acf193931b75f951cb
Matias Szamrynskyj <matiastoo@hotmail.com>
2022-04-04 19:02:18 -0300

    Sonidos owo

--- Assets/Hernan/InteractableObject.cs
+++ Assets/Hernan/InteractableObject.cs
@@ -7,8 +7,12 @@ public class InteractableObject : MonoBehaviour, IHitabble
 {
     public UnityEvent imDie;
 
+    public AudioSource maderaSound;
+
     public void ReciveHit()
     {
         imDie.Invoke();
+        if (tag == "Madera")
+            maderaSound.Play();
     }
 }
