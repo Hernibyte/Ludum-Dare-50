94f2b825558e7119c319548e87ed11a6144baffd
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-04-04 16:29:11 -0300

    Added excuses counter

--- Assets/Hernan/ExcuseBar.cs
+++ Assets/Hernan/ExcuseBar.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class ExcuseBar : MonoBehaviour
 {
+    public Sprite[] imagenes;
     public Image BarraProgreso;
 
     public void Actualizarbarra(int excusasactuales)
     {
-        int porcentaje = excusasactuales / 10;
-        BarraProgreso.fillAmount = porcentaje;
+        BarraProgreso.sprite = imagenes[excusasactuales];
     }
 
     // Start is called before the first frame update
