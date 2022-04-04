43a75719083ba78dbb5ec3be1eb7579174e4135f
Lucio <luciomolinajalabert@live.com.ar>
2022-04-04 14:52:10 -0300

    excusesBar

---
+++ Assets/Hernan/ExcuseBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExcuseBar : MonoBehaviour
+{
+    public Image BarraProgreso;
+
+    public void Actualizarbarra(int excusasactuales)
+    {
+        int porcentaje = excusasactuales / 10;
+        BarraProgreso.fillAmount = porcentaje;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
