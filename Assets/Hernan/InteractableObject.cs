5fbb6c38710fe619682bcce1d64bea2c304c1ede
Matias Szamrynskyj <matiastoo@hotmail.com>
2022-04-04 19:11:18 -0300

    Chad sound

--- Assets/Hernan/InteractableObject.cs
+++ Assets/Hernan/InteractableObject.cs
@@ -8,11 +8,14 @@ public class InteractableObject : MonoBehaviour, IHitabble
     public UnityEvent imDie;
 
     public AudioSource maderaSound;
+    public AudioSource chadSound;
 
     public void ReciveHit()
     {
         imDie.Invoke();
         if (tag == "Madera")
             maderaSound.Play();
+        else if(tag == "Chad")
+            chadSound.Play();
     }
 }
